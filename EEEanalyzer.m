6f2b0265bf4a9899c557f9a347e43b8841f7b939
Fabio Pinciroli <fabiusp98@gmail.com>
2016-04-09 14:46:29 +0200

    Aggintui file

---
+++ EEEanalyzer.m
@@ -0,0 +1,152 @@
+%E.E.E.-analyzer - MAIN by Fabio Pinciroli
+%DISTRIBUTED UNDER GPL V3 LICENSE
+%TODO:
+%<26>vedere per problema di certificati sul sito web dqm
+%<48>mettere apposto label file report
+
+[fName, fDir] = uigetfile('*.csv', 'Seleziona file');                      %chiedi nome file
+tic();
+mkdir(fDir, fName(1: length(fName) - 4));                                  %fai cartella col nome del file
+movefile(fullfile(fDir, fName), strcat(fDir,strcat('/', fName(1: length(fName) - 4))));    %muovici detro il file csv
+
+fDir = strcat(fDir, strcat(fName(1: length(fName) - 4), '\'));             %aggiorna fDir alla locazione attuale
+
+fRep = fopen(fullfile(fDir, 'REPORT.txt'), 'wt');                          %apri file report numerico
+
+%importa dati--------------------------------------------------------------
+dati = csvread(fullfile(fDir, fName));                                     
+
+%estrai nome telescopio, giorno, mese, anno--------------------------------
+tName = fName(1: length(fName) - 21);                                      
+
+tYear = fName(length(fName) - 19: length(fName) - 16);
+
+tMonth = fName(length(fName) - 14: length(fName) - 13);
+
+tDay = fName(length(fName) - 11: length(fName) - 10);
+
+tDate = fName(length(fName) - 19: length(fName) - 10);
+
+%salva report dqm----------------------------------------------------------
+%websave([fDir 'dqmReport'], strcat('https://www1.cnaf.infn.it/eee/monitor//dqmreport/', strcat(tName, strcat('/', strcat(tDate, '/')))));
+%FIXARE PROBLEMA DEI CERTIFICATI!!!
+%http://dotbootstrap.x2q.net/java-default-keystore-password-cacerts/
+%http://it.mathworks.com/matlabcentral/answers/92506-can-i-force-urlread-and-other-matlab-functions-which-access-internet-websites-to-open-secure-websi http://it.mathworks.com/matlabcentral/answers/39563-managing-public-key-certificates-in-matlab
+
+format long;                                                               %imposta display a piena risoluzione
+
+%Statistiche di base X, Y, Z, chi^2, TOF, e lunghezza traccia--------------
+GraphStats(fRep, fDir, dati, 7, 'X', 0);
+GraphStats(fRep, fDir, dati, 8, 'Y', 0);
+GraphStats(fRep, fDir, dati, 9, 'Z', 0);
+GraphStats(fRep, fDir, dati, 10, 'Chi^2', 0);
+GraphStats(fRep, fDir, dati, 11, 'TOF', 0);
+GraphStats(fRep, fDir, dati, 12, 'Track lenght', 0);
+
+%Differenza colonne tempi--------------------------------------------------
+[varA, varB] = size(dati);                                                 %calcola dimensioni array
+fprintf(fRep, '\nRun duration in senconds: %f\n', dati(varA, 4) - dati(1, 4)); 
+fprintf(fRep, '!!!VAL_COL_5: %f\n', dati(varA, 5) - dati(1, 5));
+fprintf(fRep, '!!!VAL_COL_6: %f\n', dati(varA, 6) - dati(1, 6));
+
+%Conta quanti chi^2 maggiori di 10-----------------------------------------
+tot = 0;
+for cnt = 1:1:varA
+    if dati(cnt, 10) > 10
+        tot = tot + 1;
+    end
+end
+fprintf(fRep, '\nHits with Chi^2 > 10: %f\n', tot);
+
+%Conta TOF negativi--------------------------------------------------------
+tot = 0;
+for cnt = 1:1:varA
+    if dati(cnt, 11) < 0
+        tot = tot + 1;
+    end
+end
+fprintf(fRep, '\nNegative TOF: %f\n', tot);
+
+%Conta TOF 0 e metti in array----------------------------------------------
+tot = 0;
+for cnt = 1:1:varA
+    if dati(cnt, 11) == 0
+        tot = tot + 1;
+        negTof(tot) = cnt;                                                 %#ok<SAGROW> %aggiungi indirizzo di ogni TOF negativo all'array                                   
+    end
+end
+fprintf(fRep, 'Null TOF: %f\n', tot);
+
+%Statistiche raggruppate di X Y e Z----------------------------------------
+figure('Name', 'Coordinates Stats');
+
+yy(1, 1) = min(dati(:,7));
+yy(1, 2) = mean(dati(:,7));
+yy(1, 3) = max(dati(:,7));
+
+yy(2, 1) = min(dati(:,8));
+yy(2, 2) = mean(dati(:,8));
+yy(2, 3) = max(dati(:,8));
+
+yy(3, 1) = min(dati(:,9));
+yy(3, 2) = mean(dati(:,9));
+yy(3, 3) = max(dati(:,9));
+
+bar(yy);
+
+xx = {'X', 'Y', 'Z'};
+set(gca, 'XTick', 1:4, 'XTickLabel', xx);
+
+
+l = cell(1,3);
+l{1} = 'Min';
+l{2} = 'Avg';
+l{3} = 'Max';
+legend(l);
+
+clear xx;
+clear yy;
+
+saveas(gcf, [fDir 'XYZ Stats.jpg']);                                       %salva in directory attuale con nome corretto in jpeg
+
+ko = 0; %DEBUG!!!
+if ko == 0                                                                 %se persistenza off chiudi finestre
+ close();
+end
+
+%Calcoli angoli------------------------------------------------------------
+for cnt = 1:1:varA 
+    dati(cnt, 13) = acos(dati(cnt, 9)); 
+end
+
+for cnt = 1:1:varA 
+    dati(cnt, 14) = dati(cnt, 8)/dati(cnt, 7); 
+end
+
+for cnt = 1:1:varA 
+    dati(cnt, 15) = atan(dati(cnt, 8)); 
+end
+
+for cnt = 1:1:varA 
+    dati(cnt, 16) = radtodeg(dati(cnt, 13)); 
+end
+
+for cnt = 1:1:varA 
+    dati(cnt, 17) = radtodeg(dati(cnt, 15)); 
+end
+
+GraphStats(fRep, fDir, dati, 16, 'Radius (deg)', 0);
+GraphStats(fRep, fDir, dati, 17, 'Azimuth (deg)', 0);
+
+fclose(fRep);
+disp('DONE');
+toc();
+
+
+
+
+
+
+
+
+
---
+++ GraphStats.m
@@ -0,0 +1,58 @@
+%E.E.E.-analyzer - GRAPH ENGINE by Fabio Pinciroli
+%DISTRIBUTED UNDER GPL V3 LICENSE
+%TODO: NONE
+
+function GraphStats(repFile, path, dati, col, name, ko)
+
+figure('Name', strcat(name, ' stats'));                                    %spawna finestra
+
+TOFmin = min(dati(:,col));                                                 %calcola valori statistici
+TOFmax = max(dati(:,col));
+TOFmedian = median(dati(:,col));
+TOFmode = mode(dati(:,col));
+TOFavg = mean(dati(:,col));
+
+fprintf(repFile, '%s min: %f\n', name, TOFmin);
+fprintf(repFile, '%s max: %f\n', name, TOFmax);
+fprintf(repFile, '%s avg: %f\n', name, TOFavg);
+fprintf(repFile, '%s mode: %f\n', name, TOFmode);
+fprintf(repFile, '%s median: %f\n\n', name, TOFmedian);
+
+
+plot(dati(:,col),'+');                                                     %stampa dati in ordine
+title(name);                                                               %imposta scritte, etc
+xlabel('Numero evento');
+ylabel(name);
+lMax = refline(0,TOFmax);                                                  %fai linea per il massimo
+lMax.Color = 'r';                                                          %imposta colore
+lMax.LineStyle = '--';                                                     %imposta stile  
+lMin = refline(0,TOFmin);                                                  %ripeti per le altre statistiche
+lMin.Color = 'b';
+lMin.LineStyle = '--';
+lMedian = refline(0,TOFmedian);
+lMedian.Color = 'g';
+lMedian.LineStyle = '--';
+lMode = refline(0,TOFmode);
+lMode.Color = 'magenta';
+lMode.LineStyle = '--';
+lAvg = refline(0,TOFavg);
+lAvg.Color = 'cyan';
+lAvg.LineStyle = '--';
+legend('Dati', strcat('Massimo = ', 32, num2str(TOFmax)), strcat('Minimo = ', 32, num2str(TOFmin)), strcat('Mediana = ', 32, num2str(TOFmedian)), strcat('Moda = ', 32, num2str(TOFmode)), strcat('Media = ', 32, num2str(TOFavg)));
+saveas(gcf, [path strcat(name, 'Stats.jpg')]);                             %salva in directory attuale con nome corretto in jpeg                                            
+
+if ko == 0                                                                 %se persistenza off chiudi finestre
+ close();
+end
+
+figure('Name', strcat(name, ' distribution'));                             %spawna finestra
+pd = fitdist(dati(:,col), 'Normal');                                       %calcola distribuzione normale
+assX = TOFmin:0.00001:TOFmax;                                              %genera indice asse x dal minimo valore al massimo e in step corretti
+plot(assX, pdf(pd, assX));                                                 %stampa curva
+title(strcat(name, ' distribution'));                                      %imposta scritte, etc
+clear assX;                                                                %cancella indice
+saveas(gcf, [path strcat(name, 'Distribution.jpg')]);                      %salva in directory attuale con nome corretto in jpeg 
+
+if ko == 0
+ close();
+end
